9a42ef3a0dc7cbec150dd71fd06ea217e20496e7
dean808 <deansingleton1999@gmail.com>
2018-02-01 16:10:57 +0000

    added code to move to signup on login fail

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,15 +1,15 @@
 <template>
     <v-flex> 
-        <v-tabs fixed centered>
-
+        <v-tabs fixed centered v-model="tab">
+        
             <v-tabs-bar style="background-color: #212121;" dark>
                 <v-tabs-slider color="purple"></v-tabs-slider>
-                <v-tabs-item href="'#tab-1"> Signup</v-tabs-item>
-                <v-tabs-item href="'#tab-2">Sign In</v-tabs-item>
+                <v-tabs-item href="#signup-tab"> Signup</v-tabs-item>
+                <v-tabs-item href="#login-tab">login</v-tabs-item>
             </v-tabs-bar>
 
             <v-tabs-items>
-                <v-tabs-content id="'tab-1">
+                <v-tabs-content id="signup-tab">
                     <v-card flat>
                         <v-form v-model="valid" ref="form" lazy-validation>
 
@@ -20,12 +20,12 @@
                     </v-card>
                 </v-tabs-content>
 
-                <v-tabs-content id="'tab-2">
+                <v-tabs-content id="login-tab">
                     <v-card flat>
                         <v-form v-model="valid" ref="form" lazy-validation>
                             <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
                             <v-text-field label="Password" v-model="password" :rules="passwordRules" required></v-text-field>
-                            <v-btn @click="login" :disabled="!valid">Sign in</v-btn>
+                            <v-btn @click="login" :disabled="!valid">login</v-btn>
                         </v-form>
                     </v-card>
                 </v-tabs-content>
@@ -39,6 +39,7 @@
 export default {
   data: () => ({
     valid: true,
+    tab:'',
     email: "",
     emailRules: [
       v => !!v || "E-mail is required",
@@ -56,18 +57,30 @@ export default {
   methods: {
     signup() {
       if (this.$refs.form.validate()) {
-        axios.post("/api/signup", {
+        axios.post("http://dirty-cups.dev.maxc.in/user/signup", {
           email: this.email,
           password: this.password
-        });
+        }).then((resp)=>{
+          if(resp.status !== 200){
+            throw new Error(resp.data.message)
+          }
+          this.tab = 'login-tab'
+          return resp.data;
+        })
       }
     },
     login() {
       if (this.$refs.form.validate()) {
-        axios.post("api/login", {
+        axios.post("http://dirty-cups.dev.maxc.in/user/login", {
           email: this.email,
           password: this.password
-        });
+        }).then((resp)=>{
+          if(resp.status !== 200){
+            throw new Error(resp.data.message)
+          }
+          this.tab = 'signup-tab'
+          return resp.data;
+        })
       }
     }
   }
